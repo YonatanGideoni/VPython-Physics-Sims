fd9ef34f68548f240a07430bdf8c130d6a3fa077
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-10-09 23:12:05 +0300

    Slight modifications to SpringPendulum.

--- SpringPendulum/main.py
+++ SpringPendulum/main.py
@@ -4,13 +4,14 @@ ZERO_VECTOR = vector(0, 0, 0)
 
 
 class SpringPendulum:
-    def __init__(self, start_pos=ZERO_VECTOR, end_pos=vector(0, 0.3, 0), mass=0.5, spring_constant=5):
-        self.spring = helix(pos=start_pos, axis=end_pos - start_pos, radius=0.1)
+    def __init__(self, start_pos=ZERO_VECTOR, end_pos=vector(0, -0.3, 0), mass=0.5, spring_constant=5):
+        self.spring = helix(pos=start_pos, axis=end_pos - start_pos, radius=0.1, color=color.green)
         self.spring_constant = spring_constant
         self.mass = mass
+        self.weight_pos = end_pos
         self.acceleration = ZERO_VECTOR
         self.velocity = ZERO_VECTOR
-        # self.weight = cylinder(pos=end_pos, axis=(end_pos - start_pos).y / 5)
+        self.weight = cylinder(pos=end_pos, axis=vector(0, (end_pos - start_pos).y / 5, 0), radius=0.1, color=color.red)
 
 
 SpringPendulum()
