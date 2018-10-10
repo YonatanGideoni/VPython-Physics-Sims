7e98a4393b2c88662d571d91ff7e0cf919f22040
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-10-10 14:27:20 +0300

    Seemingly working kinematics, graphless.

--- SpringPendulum/main.py
+++ SpringPendulum/main.py
@@ -1,9 +1,8 @@
 from visual import *
 
 # CONSTANTS #
-ZERO_VECTOR = vector(0, 0, 0)
 GRAPHS_ENABLED = false
-DT = 1E-20
+DT = 1E-3
 G = vector(0, -9.81, 0)
 
 # STARTING TERMS #
@@ -19,7 +18,7 @@ class Energy:
 
 
 class SpringPendulum:
-    def __init__(self, equilibrium_point=ZERO_VECTOR, start_pos=ZERO_VECTOR, end_pos=vector(0, -0.3, 0), mass=0.5,
+    def __init__(self, equilibrium_length=0, start_pos=vector(0, 0, 0), end_pos=vector(0, -0.3, 0), mass=0.5,
                  spring_constant=5):
         self.spring = helix(pos=start_pos, axis=end_pos - start_pos, radius=0.1, color=color.green)
         self.spring_constant = spring_constant
@@ -28,17 +27,19 @@ class SpringPendulum:
         self.pos = self.weight_pos
         self.energy = Energy()
         self.gravity_enabled = true
-        self.acceleration = ZERO_VECTOR
-        self.velocity = ZERO_VECTOR
-        self.equilibrium_point = equilibrium_point
-        self.weight = cylinder(pos=end_pos, axis=vector(0, (end_pos - start_pos).y / 5, 0), radius=0.1, color=color.red)
+        self.acceleration = vector(0, 0, 0)
+        self.velocity = vector(0, 0, 0)
+        self.equilibrium_length = equilibrium_length
+        self.weight = cylinder(pos=end_pos, axis=vector(0, (end_pos - start_pos).y / 5, 0), radius=0.1, color=color.red,
+                               make_trail=true, retain=500)
 
     def kinematics(self, dt=DT):  # Euler integration
-        self.acceleration = ZERO_VECTOR
+        self.acceleration = vector(0, 0, 0)
         if self.gravity_enabled:
             self.acceleration += self.mass * G
 
-        self.acceleration += self.spring_constant * (self.weight_pos - self.equilibrium_point)
+        self.acceleration += -self.spring_constant * (
+                mag(self.weight_pos) - self.equilibrium_length) * self.weight_pos / mag(self.weight_pos)
         self.velocity += self.acceleration * dt
         self.pos += self.velocity * dt
 
@@ -50,12 +51,12 @@ class SpringPendulum:
 
 # ANIMATION
 
-test_spring = SpringPendulum()
+test_spring = SpringPendulum(end_pos=vector(0, -0.3, 0))
 
-while t < 5:
+while true:
     t += DT
 
-    rate(10)
+    rate(1000)
 
     test_spring.kinematics()
     test_spring.update_pos()
