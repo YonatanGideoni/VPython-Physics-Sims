8e18b1bec32816526bde5222dec189c73b4a6719
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-11-01 22:06:15 +0200

    WIP Writing data to excel file.

--- SpringPendulum/main.py
+++ SpringPendulum/main.py
@@ -1,12 +1,17 @@
 from visual import *
+import xlwt
 
 # CONSTANTS #
 GRAPHS_ENABLED = false
-DT = 1E-3
+real_dt = 0.03
+DT = 1E-3 * real_dt
 G = vector(0, -9.81, 0)
 
 # STARTING TERMS #
 t = 0
+
+book = xlwt.Workbook('Data sheet')
+sheet1 = book.add_sheet('Data')
 
 
 # CLASSES #
@@ -18,9 +23,9 @@ class Energy:
 
 
 class SpringPendulum:
-    def __init__(self, equilibrium_length=0, start_pos=vector(0, 0, 0), end_pos=vector(0, -0.3, 0), mass=0.5,
-                 spring_constant=5):
-        self.spring = helix(pos=start_pos, axis=end_pos - start_pos, radius=0.1, color=color.green)
+    def __init__(self, equilibrium_length=0.2, start_pos=vector(0, 0, 0), end_pos=vector(0, -0.3, 0), mass=0.25,
+                 spring_constant=30, trail_retain=5000, radius=0.1):
+        self.spring = helix(pos=start_pos, axis=end_pos - start_pos, radius=radius, color=color.green)
         self.spring_constant = spring_constant
         self.mass = mass
         self.weight_pos = end_pos
@@ -30,8 +35,9 @@ class SpringPendulum:
         self.acceleration = vector(0, 0, 0)
         self.velocity = vector(0, 0, 0)
         self.equilibrium_length = equilibrium_length
-        self.weight = cylinder(pos=end_pos, axis=vector(0, (end_pos - start_pos).y / 5, 0), radius=0.1, color=color.red,
-                               make_trail=true, retain=500)
+        self.weight = cylinder(pos=end_pos, axis=vector(0, -radius, 0), radius=radius,
+                               color=color.red,
+                               make_trail=true, retain=trail_retain)
 
     def kinematics(self, dt=DT):  # Euler integration
         self.acceleration = vector(0, 0, 0)
@@ -51,10 +57,14 @@ class SpringPendulum:
 
 # ANIMATION
 
-test_spring = SpringPendulum(end_pos=vector(0, -0.3, 0))
-
+test_spring = SpringPendulum(radius=0.05, end_pos=vector(0, -0.09, 0.04), mass=0.2, spring_constant=35,
+                             equilibrium_length=0.17)
+print('x y z t')
 while true:
     t += DT
+
+    if t % real_dt < DT:  # so it works with the slight floating point precision errors
+        print(test_spring.pos.x, test_spring.pos.y, test_spring.pos.z, t)
 
     rate(1000)
 
