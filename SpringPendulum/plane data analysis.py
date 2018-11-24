d51242a4d8ea62bae5e07a39de18248936284c42
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-11-24 21:55:59 +0200

    Added work-plan.

--- SpringPendulum/plane data analysis.py
+++ SpringPendulum/plane data analysis.py
@@ -3,8 +3,29 @@
 # a continuous manner. Here, I analyze whether the angle between consecutive planes and the time it takes for
 # the pendulum to change its plane of motion are constant.
 
+# Things that the code needs to do, in order:
+# 0. Create x(z) chart, add to results
+# 1. Calculate "instability" data, AKA distance from origin for every point.
+# 2. Find all local maximum instability points.
+# 3. Find local maximums' local maximum points.
+# 4. Use these points to identify time-ranges during which the pendulum is swinging in a certain plane.
+# 5. Calculate average instability.
+# 6. Use average instability to filter out close points (say, all points with under-average stability) as they
+#    do not represent well what plane the pendulum is swinging in.
+# 7. Get lists of points per plane.
+# 8. Use SciPy linear regression to find slope and r^2 of plane.
+# 9. Calculate angle of every plane.
+# 10. Calculate change of angle between planes.
+# 11. Print to results slope, r^2, plane angle, change of angle between planes, and transition time between planes.
+# 12. Graph change of angle between planes and transition time, add to results.
+# 13. Save excel file and hope that the physics works just as well as the code.
 
 import openpyxl as pyxl
 import numpy as np
-from scipy import stats
+from scipy import stats  # used only for linear regression, to estimate angle of plane.
 
+wb = pyxl.load_workbook('test_motion_data.xlsx')
+results_sheet = wb.create_sheet('Results')
+data_sheet = wb.get_sheet_by_name('Data')
+
+# TODO: create basic x(z) chart
